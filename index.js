99bc76694558d9173d3c6b425c794c19712dcde8
Ilaria Callegari <ilaria.callegari@gmail.com>
2020-10-14 17:09:52 +0100

    modify get and post signup route handlers, add signout route handler and get and post signin route hadlers

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const cookieSession = require("cookie-session");
 const usersRepo = require("./repositories/users.js");
 const app = express();
 
-
 //applies body parser to every requests but not to get request
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //wire up cookie-session
-app.use(cookieSession({
-  keys: ['wedqewfvreqg'],
-
-}))
+app.use(
+  cookieSession({
+    keys: ["wedqewfvreqg"],
+  })
+);
 
 //route handler
-app.get("/", (req, res) => {
+app.get("/signup", (req, res) => {
   res.send(`
     <div>
     Your user id is: ${req.session.userId}
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
     `);
 });
 
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
   const existingUser = await usersRepo.getOneBy({ email });
   if (existingUser) {
@@ -41,8 +41,38 @@ app.post("/", async (req, res) => {
   //create a user in our user repo to represent this person
   const user = await usersRepo.create({ email, password });
   //store the id of that user inside the user cookie
-  req.session.userId = user.id; 
+  req.session.userId = user.id;
   res.send("Account created");
+});
+
+app.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("you are logged out");
+});
+
+app.get("/signin", (req, res) => {
+  res.send(`
+  <div>
+      <form method="POST">
+          <input placeholder="email" name= "email" /> 
+          <input placeholder="password" name= "password" /> 
+          <button> Sign in </button>
+      </form>
+  </div> 
+  `);
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await usersRepo.getOneBy({ email });
+  if (!user) {
+    return res.send("There is no user signed up with this email");
+  }
+  if (user.password !== password) {
+    return res.send("Password incorrect!");
+  }
+  req.session.userId = user.id;
+  res.send("Yuo are successfully signed in!");
 });
 
 //Port
