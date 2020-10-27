cc8fbcd1a738d9f4e10d1ce1be68748ba2f462ac
Ilaria Callegari <ilaria.callegari@gmail.com>
2020-10-27 11:33:36 +0000

    add middleware function to handle errors to signup/signin/products routes

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { body, validationResult } = require("express-validator");
 const usersRepo = require("../../repositories/users");
+
+const { handleErrors } = require("./middlewares");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
 const {
@@ -20,13 +21,9 @@ router.get("/signup", (req, res) => {
 
 router.post(
   "/signup",
-  [requireEmail, requirePassword, requirePasswordConfirmation],
+  [requireEmail, requirePassword, requirePasswordConfirmation], handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
     //create a user in our user repo to represent this person
     const user = await usersRepo.create({ email, password });
     //store the id of that user inside the user cookie
@@ -46,14 +43,8 @@ router.get("/signin", (req, res) => {
 
 router.post(
   "/signin",
-  [
-    requireEmailExists, requireValidPasswordForUser
-  ],
+  [requireEmailExists, requireValidPasswordForUser], handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-      return res.send(signinTemplate({errors}))
-    }
     const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });
     req.session.userId = user.id;
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { validationResult } = require("express-validator");
 const multer = require('multer');
 const products = require("../../repositories/products");
 
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
+const {handleErrors} = require('./middlewares');
 
 const router = express.Router();
 const upload = multer({storage: multer.memoryStorage()}); //destination of the files that are being uploaded
@@ -16,11 +16,7 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", upload.single('image'), [requireTitle, requirePrice], async(req, res) => {
-  const errors = validationResult(req); //get out of the requests only the errors
-  if(!errors.isEmpty()){
-    res.send(productsNewTemplate({errors}));
-  }
+router.post("/admin/products/new", upload.single('image'), [requireTitle, requirePrice], handleErrors(productsNewTemplate), async(req, res) => {
   const image = req.file.buffer.toString('base64');
   const {title, price} = req.body;
   const product = await productsRepo.create({title, price, image});
